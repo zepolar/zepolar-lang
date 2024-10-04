773045c9b6ad39434f3b0f9a451ad97948b8b372
Alexander López <zepolar@gmail.com>
2024-10-03 19:52:07 -0500

    Adding test

--- src/test/java/com/linbrox/common/DateUtilsTest.java
+++ src/test/java/com/linbrox/common/DateUtilsTest.java
@@ -1,8 +1,15 @@
 package com.linbrox.common;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,12 +30,29 @@ class DateUtilsTest {
     }
 
 
-    @Test
-    void shouldReturnTrueWhenDateIsPast(){
-        LocalDate localDate = LocalDate.now().minusDays(1);
-        var actual = DateUtils.isPastDate(localDate.atStartOfDay());
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "2023-01-01T00:00:00",
+            "2022-12-31T23:59:59",
+            "2000-06-15T12:34:56",
+            "2019-07-20T10:15:30",
+            "1999-12-31T23:59:59"
+            // You can add more past dates here
+    })
+    void shouldReturnTrueWhenDateTimeStringIsPast(String dateTimeStr){
+        var actual = DateUtils.isPastDate(dateTimeStr);
         assertTrue(actual);
     }
+
+    @Test
+    void shouldThrownAnErrorWhenDateTimeStringIsNull(){
+        String dateTimeStr = null;
+        assertThrows(Exception.class, ()-> DateUtils.isPastDate(dateTimeStr));
+
+    }
+
+
+
 
 
 }
