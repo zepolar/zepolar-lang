ef820147ffb46faeefa2d9628f1ce578f55106c1
Josue Ordonez <razuordonez@gmail.com>
2024-10-04 23:03:49 -0500

    two methods implemented in NumericUtils with them respective tests

--- src/main/java/com/linbrox/common/NumericUtils.java
+++ src/main/java/com/linbrox/common/NumericUtils.java
@@ -16,9 +16,30 @@ public final class NumericUtils {
         return Double.valueOf(value);
     }
 
-    
     public static BigDecimal toBigDecimal(String value){
         return new BigDecimal(value);
+    }
+
+    /**
+     * Converts a String to a Long.
+     *
+     * @param value the String to be converted
+     * @return the Long representation of the String
+     * @throws NumberFormatException if the string cannot be parsed as a Long
+     */
+    public static Long toLong(String value) {
+        return Long.valueOf(value);
+    }
+
+    /**
+     * Converts a String to a Float.
+     *
+     * @param value the String to be converted
+     * @return the Float representation of the String
+     * @throws NumberFormatException if the string cannot be parsed as a Float
+     */
+    public static Float toFloat(String value) {
+        return Float.valueOf(value);
     }
 
 }
